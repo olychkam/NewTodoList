5c800376c23e1651548bf1aa12e10ed363611327
olychkam <olkaaamartynova@gmail.com>
2021-07-21 21:18:46 +0300

    Updates

---
+++ precache-manifest.56fe24de891194f0dae03185990c795f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2c51d8be9c3181e13cfc3f4c17bc47",
+    "url": "/NewTodoList/index.html"
+  },
+  {
+    "revision": "8e4466f762318ea845e2",
+    "url": "/NewTodoList/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "668e3d3405b2b57c2843",
+    "url": "/NewTodoList/static/js/2.e7aa4cdc.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/NewTodoList/static/js/2.e7aa4cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4466f762318ea845e2",
+    "url": "/NewTodoList/static/js/main.9121a7a4.chunk.js"
+  },
+  {
+    "revision": "fe97ef35929ed78b4bb1",
+    "url": "/NewTodoList/static/js/runtime-main.e9e28436.js"
+  }
+]);
